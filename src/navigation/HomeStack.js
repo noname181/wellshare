9e659769b1d426f6739f40d760f1254a529f8d35
oniiyachan <jhelios1010@gmail.com>
2021-11-18 17:09:25 +0700

    hao

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Store from '../screens/Store';
 import Detail from '../screens/Detail';
 import Main from '../screens/Main';
+import Hospital_42_1 from '../screens/Hospital_42_1';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ function HomeStack() {
             <Stack.Screen name="Main" component={Main} />
             <Stack.Screen name="Store" component={Store} />
             <Stack.Screen name="Detail" component={Detail} />
+            <Stack.Screen name="Hospital_42_1" component={Hospital_42_1} />
         </Stack.Navigator>
     );
 }
