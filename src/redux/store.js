7fc0f22d197c344e2e6929e31c4cafe097c7cb5f
Noname <l.h.thuong181@gmail.com>
2021-11-22 13:12:13 +0700

    redux

---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+// Imports: Dependencies
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+// Imports: Redux Root Reducer
+import rootReducer from './reducers/index';
+
+
+// Redux: Store
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunk,
+        // createLogger(),
+    ),
+);
+
+// Exports
+export {
+    store,
+}
