4a5dcddd5532746b308aad06bdea66c966f87a72
huynhnhutlinh99 <40021780+huynhnhutlinh99@users.noreply.github.com>
2021-11-24 15:30:39 +0700

    cs

--- src/navigation/Admin_TabNavigation.js
+++ src/navigation/Admin_TabNavigation.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import Main from '../screens/Admin/Main'
-import { View, StyleSheet, Text } from 'react-native'
-import List from '../images/svg/List'
-import Notice from '../images/svg/Notice'
-import Profile_Icon from '../images/svg/Profile'
+import React, { useState, useEffect } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, StyleSheet, Text, InteractionManager } from 'react-native';
+import Header from '../components/Header';
+import Screen from '../components/Screen';
+//Screen
+import Main from '../screens/Admin/Main';
+import NoticeScreen from '../screens/Admin/Notice';
+import ListViewScreen from '../screens/Admin/ListView';
+//Images
+import List from '../images/svg/List';
+import Notice from '../images/svg/Notice';
+import Profile_Icon from '../images/svg/Profile';
 
 
 
@@ -24,17 +30,15 @@ function TabNavigation({ route, navigation }) {
             }}
         >
             <Tab.Screen
-                name="Main"
-                component={Main}
+                name="ListView"
+                component={ListViewScreen}
                 options={{
-                    headerShown: false,
                     tabBarIcon: ({ focused }) => (<View style={styles.tab}>
                         <List height={19} width={19} color={focused ? '#7c257a' : '#b2b2b2'} />
                         <Text style={[styles.text, { color: focused ? '#7c257a' : '#b2b2b2' }]}>명단</Text>
                     </View>),
-
+                    header: props => <Header title="명단" isBack={false} {...props} />
                 }}
-
             />
             <Tab.Screen
                 name="Main2"
@@ -45,12 +49,25 @@ function TabNavigation({ route, navigation }) {
                         <Notice height={19} width={19} color={focused ? '#7c257a' : '#b2b2b2'} />
                         <Text style={[styles.text, { color: focused ? '#7c257a' : '#b2b2b2' }]}>명단</Text>
                     </View>),
-
+                    
                 }}
 
             />
             <Tab.Screen
-                name="Main3"
+                name="Notice"
+                component={NoticeScreen}
+                options={{
+                    // headerShown: false,
+                    tabBarIcon: ({ focused }) => (<View style={styles.tab}>
+                        <Notice height={19} width={19} color={focused ? '#7c257a' : '#b2b2b2'} />
+                        <Text style={[styles.text, { color: focused ? '#7c257a' : '#b2b2b2' }]}>명단</Text>
+                    </View>),
+                    header: props => <Header title="공지사항" isBack={false} {...props} />
+                }}
+
+            />
+            <Tab.Screen
+                name="Main4"
                 component={Main}
                 options={{
                     headerShown: false,
@@ -58,7 +75,7 @@ function TabNavigation({ route, navigation }) {
                         <Profile_Icon height={19} width={25} color={focused ? '#7c257a' : '#b2b2b2'} />
                         <Text style={[styles.text, { color: focused ? '#7c257a' : '#b2b2b2' }]}>명단</Text>
                     </View>),
-
+                    
                 }}
 
             />
