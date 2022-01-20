1ad22de8e19e8daa36a87883009b93220dd361dd
Noname <l.h.thuong181@gmail.com>
2022-01-20 23:28:16 +0700

    .

--- src/screens/DeliveryMan/List.js
+++ src/screens/DeliveryMan/List.js
@@ -8,48 +8,15 @@ import {
     TextInput,
     Button,
     TouchableOpacity,
-    FlatList
-
+    FlatList,
+    Alert
 } from 'react-native';
 import Screen from '../../components/Screen';
 import Calendar from '../../images/svg/CalendarIcon';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { useSelector } from 'react-redux'
 import axios from '../../helpers/axiosInterceptor';
-
-const DATA = [
-    {
-        id: '1',
-        regdate: '2022-01-02',
-        name: '조형래 ',
-        phone: '010-2009-7723',
-        active: true
-    },
-    {
-        id: '2',
-        regdate: '2022-01-02',
-        name: '김상수 ',
-        phone: '010-5242-4213',
-        active: false
-    },
-    {
-        id: '3',
-        regdate: '2022-01-02',
-        name: '강우식 ',
-        phone: '010-5213-3423',
-        active: false
-    },
-    {
-        id: '4',
-        regdate: '2022-01-02',
-        name: '이명상 ',
-        phone: '010-5324-3231',
-        active: false
-    },
-
-];
-
-
+import BackgroundGeolocation from '@mauron85/react-native-background-geolocation';
 
 
 function List({ navigation }) {
@@ -64,6 +31,7 @@ function List({ navigation }) {
 
     useEffect(() => {
         console.log(user)
+        TrackingInit();
         axios.post(`/user_load_bookings.php`, { d_no: user.d_no, role: 'delivery' })
             .then(res => {
                 setBookings(res.data.bookings)
@@ -76,6 +44,118 @@ function List({ navigation }) {
 
         };
     }, []);
+
+    const TrackingInit = () => {
+        BackgroundGeolocation.configure({
+            desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
+            stationaryRadius: 30,
+            distanceFilter: 30,
+            notificationTitle: 'Delivery Tracking',
+            notificationText: 'Enabled',
+            debug: false,
+            startOnBoot: false,
+            stopOnTerminate: true,
+            locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
+            interval: 600000,
+            fastestInterval: 600000,
+            activitiesInterval: 10000,
+            stopOnStillActivity: false,
+            startForeground: true,
+            url: 'https://scsman23.cafe24.com/api/tracking_delivery.php',
+            httpHeaders: {
+                'X-FOO': 'bar',
+                'Content-Type': 'application/json'
+            },
+            // customize post properties
+            postTemplate: {
+                lat: '@latitude',
+                lng: '@longitude',
+                d_no: user.d_no // you can also add your own properties
+            }
+        });
+
+        BackgroundGeolocation.on('location', (location) => {
+            // handle your locations here
+            // to perform long running operation on iOS
+            // you need to create background task
+            // Alert.alert("Modio", location);
+            BackgroundGeolocation.startTask(taskKey => {
+                // execute long running task
+                // eg. ajax post location
+                // IMPORTANT: task has to be ended by endTask
+                // axios.post(`/tracking_delivery.php`, { d_no: user.d_no, lat: location.latitude, lng: location.longitude, type: 'update' })
+                //     .then(res => {
+
+                //     })
+                //     .catch(err => {
+                //         console.log(err);
+                //     })
+                BackgroundGeolocation.endTask(taskKey);
+            });
+        });
+
+        BackgroundGeolocation.on('stationary', (stationaryLocation) => {
+            // handle stationary locations here
+            console.log(stationaryLocation)
+            //Actions.sendLocation(stationaryLocation);
+        });
+
+        BackgroundGeolocation.on('error', (error) => {
+            console.log('[ERROR] BackgroundGeolocation error:', error);
+        });
+
+        BackgroundGeolocation.on('start', () => {
+            console.log('[INFO] BackgroundGeolocation service has been started');
+        });
+
+        BackgroundGeolocation.on('stop', () => {
+            console.log('[INFO] BackgroundGeolocation service has been stopped');
+        });
+
+        BackgroundGeolocation.on('authorization', (status) => {
+            console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
+            if (status !== BackgroundGeolocation.AUTHORIZED) {
+                // we need to set delay or otherwise alert may not be shown
+                setTimeout(() =>
+                    Alert.alert('App requires location tracking permission', 'Would you like to open app settings?', [
+                        { text: 'Yes', onPress: () => BackgroundGeolocation.showAppSettings() },
+                        { text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel' }
+                    ]), 1000);
+            }
+        });
+
+        BackgroundGeolocation.on('background', () => {
+            console.log('[INFO] App is in background');
+        });
+
+        BackgroundGeolocation.on('foreground', () => {
+            console.log('[INFO] App is in foreground');
+        });
+
+        BackgroundGeolocation.on('abort_requested', () => {
+            console.log('[INFO] Server responded with 285 Updates Not Required');
+
+            // Here we can decide whether we want stop the updates or not.
+            // If you've configured the server to return 285, then it means the server does not require further update.
+            // So the normal thing to do here would be to `BackgroundGeolocation.stop()`.
+            // But you might be counting on it to receive location updates in the UI, so you could just reconfigure and set `url` to null.
+        });
+
+        BackgroundGeolocation.on('http_authorization', () => {
+            console.log('[INFO] App needs to authorize the http requests');
+        });
+
+        BackgroundGeolocation.checkStatus(status => {
+            console.log('[INFO] BackgroundGeolocation service is running', status.isRunning);
+            console.log('[INFO] BackgroundGeolocation services enabled', status.locationServicesEnabled);
+            console.log('[INFO] BackgroundGeolocation auth status: ' + status.authorization);
+
+            // you don't need to check status before start (this is just the example)
+            if (!status.isRunning) {
+                BackgroundGeolocation.start(); //triggers start on start event
+            }
+        });
+    }
 
     const onChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
